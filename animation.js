02ecaaf0bc8095c515a89beed4d36b7b58ca89c4
philip fraser <pr.fraser3@gmail.com>
2017-08-18 19:08:21 +1000

    added js file, added basic flip function, changed boxes to have full border again

---
+++ animation.js
@@ -0,0 +1,6 @@
+function toggleSpin() {
+	document.getElementById("middle-top-box").classList.toggle("flipped");
+	document.getElementById("left-middle-box").classList.toggle("flipped");
+	document.getElementById("right-middle-box").classList.toggle("flipped");
+	document.getElementById("middle-bottom-box").classList.toggle("flipped");
+}
